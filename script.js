8918060dcf2694073ea7cf134c4335ef96561f92
MisssChipi <162648763+MisssChipi@users.noreply.github.com>
2025-11-22 03:38:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const imagenes = [
+  "assets/hola_1_1.gif",
+  "assets/hola_2.gif",
+  "assets/ochocaras_1_1.gif",
+  "assets/ochocaras_2.gif",
+  "assets/ochocaras_3.gif",
+  "assets/protopix1.gif",
+  "assets/protopix2.gif",
+  "assets/protopix3.gif",
+  "assets/protopix4.gif",
+  "assets/fronteraviva.gif",
+  "assets/encuentro_3.gif",
+  
+];
+
+const preloads = [];
+
+imagenes.forEach(src => {
+  const img = new Image();
+  img.src = src;
+  img.onload = () => console.log("Cargada:", src);
+  img.onerror = () => console.log("ERROR al cargar:", src);
+
+  preloads.push(img);
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   const gif = document.getElementById("gif");
 
